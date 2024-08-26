8db241cd5499ab1e2e6b96b26d2755b0b9b2415d
unknown <tufyal15-133@diu.edu.bd>
2024-08-26 17:16:53 +0600

    unit test & spring boot setup

---
+++ src/main/java/com/example/flipcarddemo/CashCard.java
@@ -0,0 +1,5 @@
+package com.example.flipcarddemo;
+
+    record CashCard(Long id, Double amount) {
+    }
+
---
+++ src/main/java/com/example/flipcarddemo/FlipcarddemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.flipcarddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlipcarddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlipcarddemoApplication.class, args);
+	}
+
+}
