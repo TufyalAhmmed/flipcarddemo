b1b77d015ded9610b152916284577ae406020b75
unknown <tufyal15-133@diu.edu.bd>
2024-08-29 21:04:07 +0600

    workable & repository connection done

--- src/main/java/com/example/flipcarddemo/CashCardController.java
+++ src/main/java/com/example/flipcarddemo/CashCardController.java
@@ -1,15 +1,26 @@
 package com.example.flipcarddemo;
+import java.util.Optional;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @RequestMapping("/cashcards")
 class CashCardController {
-    @GetMapping("/{requestedId}")
-    private ResponseEntity<CashCard> findById() {
-        CashCard cashCard = new CashCard(99L, 123.45);
-        return ResponseEntity.ok(cashCard);
-     }
-}
+    private CashCardRepository cashCardRepository;
+//
+private CashCardController(CashCardRepository cashCardRepository) {
+        this.cashCardRepository = cashCardRepository;
+    }
+        @GetMapping("/{requestedId}")
+        private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
+            Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestedId);
+            if (cashCardOptional.isPresent()) {
+                return ResponseEntity.ok(cashCardOptional.get());
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        }
+    }
